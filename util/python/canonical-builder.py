616092c0ddabbe15205d9ddd3ef18bd5cdc364e9
Ashish Tyagi <ashishtyagi10@gmail.com>
2021-12-20 22:05:31 -0500

    generate util to replace string

--- util/python/canonical-builder.py
+++ util/python/canonical-builder.py
@@ -1,22 +1,30 @@
 import csv
 from typing import OrderedDict
 
+# load the properties file
+import myconfig
+print("myconfig.properties loaded")
+print(myconfig.pain001_name)
+
 # open the csv file and load it as dictionary
 mymapping = {}
 with open("convert.csv", "r") as csvfile:
     reader = csv.reader(csvfile)
     mymapping = {rows[0]: rows[1] for rows in reader}
 
-print(mymapping)
+print("========== all mappings are loaded ==========")
+for key, value in mymapping.items():
+    print(key, '==> ', value)
 
 
 # search and replace the string in the file
 temp_file = open("it1.txt", "r+")
 
+print("========== start to search and replace the json schema ==========")
 with open("it1.txt", "r+") as f:
     for line in f:
         for key, value in mymapping.items():
             line = line.replace(key, value)
         temp_file.write(line)
 
-print("Done")
+print("============ program end ===============")
